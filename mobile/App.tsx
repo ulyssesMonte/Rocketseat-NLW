b14340e435215cf2f2213e40dd877b7436401f92
Ulysses Monte <ulyssesmontealverne@hotmail.com>
2022-09-16 16:07:40 -0300

    uploading mobile

---
+++ mobile/App.tsx
@@ -0,0 +1,31 @@
+import { StatusBar } from "react-native";
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_700Bold,
+  Inter_900Black,
+} from "@expo-google-fonts/inter";
+
+import { Home } from "./src/screens/Home";
+import { Background } from "./src/components/background";
+import { Loading } from "./src/components/Loading";
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_700Bold,
+    Inter_900Black,
+  });
+
+  return (
+    <Background>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+
+      {fontsLoaded ? <Home /> : <Loading />}
+    </Background>
+  );
+}
